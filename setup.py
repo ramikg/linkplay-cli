14253788a8b6f8d5f09a2c195ef658394c5568eb
ramikg <72725910+ramikg@users.noreply.github.com>
2024-01-21 18:46:06 +0200

    Bump version to 0.0.6

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(name='linkplay-cli',
       long_description=long_description,
       long_description_content_type='text/markdown',
       url='https://github.com/ramikg/linkplay-cli',
-      version='0.0.5',
+      version='0.0.6',
       packages=find_packages(),
       install_requires=[
           'async_upnp_client',
